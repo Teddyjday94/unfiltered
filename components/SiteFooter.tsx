0f7d6c9353cdc751da641fbe4b14e4520fec75e2
Teddyjday94 <130117910+Teddyjday94@users.noreply.github.com>
2026-09-18 23:42:48 -0500

    Add shared site footer

---
+++ components/SiteFooter.tsx
@@ -0,0 +1,9 @@
+export default function SiteFooter() {
+  return (
+    <footer>
+      <div className="footer-mark">Z + H <span>UNFILTERED</span></div>
+      <p>Unofficial fan-made concept. Not affiliated with Zane & Heath or their team.</p>
+      <p>Public editorial/podcast imagery is used for this noncommercial mockup; use owned or licensed media for a commercial release.</p>
+    </footer>
+  );
+}
